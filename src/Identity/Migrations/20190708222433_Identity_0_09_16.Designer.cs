2e3f6f311aa6b36fcf0fcaaef941fab2e4c685e5
Babat <babat42@gmail.com>
2019-07-09 00:30:02 +0200

    Update version to 0.9.16

--- src/Identity/Migrations/20190708222433_Identity_0_09_16.Designer.cs
+++ src/Identity/Migrations/20190708222433_Identity_0_09_16.Designer.cs
@@ -4,14 +4,16 @@ using Kastra.Web.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kastra.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190708222433_Identity_0_09_16")]
+    partial class Identity_0_09_16
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
